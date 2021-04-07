e3f61f95fad3656e4ea6b681df2454fe640b6d0a
nguyenquangtho <nqtho97@gmail.com>
2021-04-08 03:35:04 +0700

    fix 3

--- src/component/mobile/searchBar.jsx
+++ src/component/mobile/searchBar.jsx
@@ -1,25 +1,6 @@
 import React from "react";
 import { AppBar, Toolbar, Container } from "@material-ui/core";
-import { makeStyles, fade } from "@material-ui/core/styles";
-import SearchInputUI from "../appBar/searchInputUI";
 import SearchProduct from "../appBar/searchProduct";
-
-const useStyle = makeStyles((theme) => ({
-  root: {
-    flexGrow: 1,
-    borderRadius: theme.shape.borderRadius,
-    color: "inherit",
-    backgroundColor: fade(theme.palette.common.white, 0.15),
-    "&:hover": {
-      backgroundColor: fade(theme.palette.common.white, 0.25),
-    },
-    padding: theme.spacing(0.5, 0.5, 0.5, 0.5),
-    margin: theme.spacing(0, -1, 0, -1),
-  },
-  searchIcon: {
-    padding: theme.spacing(0, 1),
-  },
-}));
 
 function SearchBar() {
   return (
